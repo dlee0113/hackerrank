49d01e3458aac17dc0eb13a5dfa5dbfdd62ece90
dlee0113 <dlee0113@gmail.com>
2016-10-11 08:01:05 -0400

    package name change

--- linkedlistdetectacycle/src/main/java/com/hackerrank/linkedlistdetectacycle/Node.java
+++ linkedlistdetectacycle/src/main/java/com/hackerrank/linkedlistdetectacycle/Node.java
@@ -1,4 +1,4 @@
-package hackerrank.linkedlistdetectacycle;
+package com.hackerrank.linkedlistdetectacycle;
 
 public class Node {
 	public int data;
--- linkedlistdetectacycle/src/main/java/com/hackerrank/linkedlistdetectacycle/Solution.java
+++ linkedlistdetectacycle/src/main/java/com/hackerrank/linkedlistdetectacycle/Solution.java
@@ -1,4 +1,4 @@
-package hackerrank.linkedlistdetectacycle;
+package com.hackerrank.linkedlistdetectacycle;
 
 public class Solution {
 	public static void main(String[] args) {
